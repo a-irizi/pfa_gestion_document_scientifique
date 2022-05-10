28d1cf75be3fbd3e182b0d79e18824dd5395980e
a-irizi <irizi.anass.00@gmail.com>
2022-05-10 23:12:28 +0100

    Fix user creation and user modification forms

--- users/forms.py
+++ users/forms.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm, ReadOnlyPasswordHashField
 
 from . import models
 
@@ -7,7 +7,20 @@ class CustomUserCreationForm(UserCreationForm):
         model = models.Utilisateur
         fields = ('email', 'prenom', 'deuxiemeNom', 'nom', 'dateNaissance')
 
+    def save(self, commit=True):
+        # Save the provided password in hashed format
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
+
 class CustomUserChangeForm(UserChangeForm):
+    """A form for updating users. Includes all the fields on
+    the user, but replaces the password field with admin's
+    disabled password hash display field.
+    """
+    password = ReadOnlyPasswordHashField()
     class Meta:
         model = models.Utilisateur
         fields = ('email', 'prenom', 'deuxiemeNom', 'nom', 'dateNaissance')
